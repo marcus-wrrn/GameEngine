97e656ad0c98ee6de2f7c20966fa2c289fc59da0
marcus-wrrn <marcuswrrn@gmail.com>
2022-05-04 17:00:59 -0400

    Set up proper checks for each asset list

--- Containers/AssetContainer.cs
+++ Containers/AssetContainer.cs
@@ -6,12 +6,94 @@ using Graphics.Assets;
 
 namespace Containers {
     public class AssetContainer {
-        public List<IAsset>             AllAssets{ get; private set; }
-        public List<IMovingAsset>       MovingObjects { get; private set; }
-        public List<ICharacterAsset>    Enemies { get; private set; }
-        public RockGuy                  Player { get; private set; }
+        public List<IAsset>             AllAssets{ get; private set; }              // All Assets will be stored in this list (useful for drawing or map wide effect)
+        public List<IAsset>             StaticAssets { get; private set; }          // Static objects go here
+        public List<IMovingAsset>       MovingAssets { get; private set; }         // Assets that move but don't have a sophistcated AI go here
+        public List<ICharacterAsset>    AllCharacters { get; private set; }         // Every Asset with an AI go here
+        public List<ICharacterAsset>    NonPlayerCharacters { get; private set; }   // All NPCs go here
+        public List<ICharacterAsset>    PlayerCharacters { get; private set; }      // All Player Characters here
 
+        // This list kinda sucks
+        public AssetContainer(List<IAsset> staticAssets, List<IMovingAsset> movingAssets, List<ICharacterAsset> nonPlayerCharacters, List<ICharacterAsset> playerCharacters) {
+            // Makes sure NPCs and PlayerCharacters are not in the same list
+            // Check validity of moving vs static assets
+            CheckStaticVsMovingAssets(staticAssets, movingAssets);
+            // Check for validity of player vs non player characters
+            CheckCharacters(nonPlayerCharacters, playerCharacters);
 
+            // Add all values to the list
+            AddToList(StaticAssets, staticAssets);
+            AddToList(MovingAssets, movingAssets);
+
+            AddToList(NonPlayerCharacters, nonPlayerCharacters);
+            AddToList(PlayerCharacters, playerCharacters);
+            // Add all characters to the All characters list
+            AddToList(AllCharacters, PlayerCharacters);
+            AddToList(AllCharacters, NonPlayerCharacters);
+            
+        }// end AssetContainer constructor
+
+        private void CheckStaticVsMovingAssets(List<IAsset> staticAssets, List<IMovingAsset> movingAssets) {
+            // Checking to see if moving assets contains a static asset
+            foreach(var movingAsset in movingAssets) {
+                if(staticAssets.Contains(movingAsset))
+                    throw new ArgumentException("An Asset cannot be both static and dynamic");
+            }
+        }// end CheckStaticVsMovingAssets()
+
+        private void CheckCharacters(List<ICharacterAsset> npcs, List<ICharacterAsset> players) {
+            foreach(var npc in players) {
+                if(players.Contains(npc))
+                    throw new ArgumentException("A Character cannot be player controlled and an npc");
+            }
+        }// end CheckCharacters()
+
+        // Add all non duplicate items to a list from another list
+        private void AddToList<T>(List<T> mainList, List<T> listToBeAdded)  {
+            foreach(T item in listToBeAdded) {
+                if(!mainList.Contains(item)) {
+                    mainList.Add(item);
+                }
+            }
+        }// end AddToList()
+
+        // AddToMainList needs its own method as the List might not have the same interface type
+        private void AddToMainList<T>(List<T> list) where T: IAsset {
+            foreach(var asset in list) {
+                if(!AllAssets.Contains(asset))
+                    AllAssets.Add(asset);
+            }
+        }// end AddToMainList()
+
+        // sorts assets by Y location (ensures that no object is drawn infront of the other)
+        private void SortMainAssetList() {
+            AllAssets.Sort((x, y) => x.Location.Y.CompareTo(y.Location.Y));
+        }// end SortMainAssetList()
+
+        private void AllAssetListConstructor() {
+            // Adds all assets into the assets list then sorts it by the Y location on the map
+            AddToMainList(MovingAssets);
+            AddToMainList(AllCharacters);
+            // Sort Asset list
+            SortMainAssetList();
+        }// end AllAssetListContructor()
+
+        private void AddAssetToMainList(IAsset asset) {
+            AllAssets.Add(asset);
+            AllAssets.Sort((x, y) => x.Location.Y.CompareTo(y.Location.Y));
+        }// end AddAssetToMainList()
+
+        public void AddPlayerAsset(ICharacterAsset asset) {
+            // If object already present in the list throw an error
+            foreach(var currAsset in NonPlayerCharacters) {
+                if(currAsset.Equals(asset))
+                    throw new DuplicateWaitObjectException("Asset already exists whithin array");
+            }
+            // If object not in the array add it to the NPC list
+            NonPlayerCharacters.Add(asset);
+            // Add asset to the main container
+            AddAssetToMainList(asset);
+        }
     }// end AssetContainer class
 }
 
